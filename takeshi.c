74ce021ab7bae9df729604a5c61bf61e05cc6a51
a5809041 <a5809041@aoyama.ac.jp>
2012-05-10 20:40:53 +0900

    Update takeshi.c

--- takeshi.c
+++ takeshi.c
@@ -19,3 +19,26 @@ int main(){
 	}
 	printf("max b sum is %d\n",bsum);
 }
+
+
+
+/*
+#define MAX_ROOP 100
+
+for(a1=2; a1<=MAX_ROOP; a1+=2){
+	for(a2=1; a2<=MAX_ROOP; a2++){
+		for(b1=2, b2=(a1*a2-2*b1)/2; 2*b1<a1*a2 && b1<=MAX_ROOP && b1*b2<=2*2*MAX_ROOP; b1+=2, b2-=2){
+			count++;
+			if(b1*b2 == 2*(a1+a2) && a1+a2 >= b1+b2){
+				printf("a1:%d, a2:%d, b1:%d, b2:%d\n",a1,a2,b1,b2);
+				if(b1+b2>bsum)
+					bsum=b1+b2;
+			}
+		}
+	}
+}
+printf("max b sum is %d\n",bsum);
+printf("%d\n",count);
+//count = 2908
+}
+*/
